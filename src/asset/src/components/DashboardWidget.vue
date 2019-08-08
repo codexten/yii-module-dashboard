725271914b4a696f2a770f56ea8d796b1b16a261
Jomon Johnson <jomon.entero@gmail.com>
2019-08-08 16:48:15 +0530

    init

--- src/asset/src/components/DashboardWidget.vue
+++ src/asset/src/components/DashboardWidget.vue
@@ -8,7 +8,7 @@
                     </div>
                 </div>
             </div>
-            <slot v-bind:message="message"></slot>
+            <slot v-bind:data="data"></slot>
         </div>
     </div>
 </template>
@@ -18,29 +18,35 @@ export default {
   name: 'dashboard-widget',
   data () {
     return {
-      message: 'hai',
-      // Array will be automatically processed with visualization.arrayToDataTable function
-      chartData: [
-        ['', '', ''],
-        ['Jan', 7, 7],
-        ['Feb', 9, 9],
-        ['Mar', 12, 12],
-        ['Apr', 6, 6],
-        ['May', 10, 10],
-        ['June', 8, 8],
-        ['July', 5, 5],
-        ['Aug', 13, 13],
-        ['Sep', 12, 12],
-        ['Oct', 10, 10],
-        ['Nov', 9, 9],
-        ['Dec', 8, 8],
-      ],
-      chartOptions: {
-        chart: {
-          title: 'Monthly Coffee Production by Country',
+      data: {
+        message: 'hai',
+        // Array will be automatically processed with visualization.arrayToDataTable function
+        chartData: [
+          ['', '', ''],
+          ['Jan', 7, 7],
+          ['Feb', 9, 9],
+          ['Mar', 12, 12],
+          ['Apr', 6, 6],
+          ['May', 10, 10],
+          ['June', 8, 8],
+          ['July', 5, 5],
+          ['Aug', 13, 13],
+          ['Sep', 12, 12],
+          ['Oct', 10, 10],
+          ['Nov', 9, 9],
+          ['Dec', 8, 8],
+        ],
+        chartOptions: {
+          chart: {
+            title: 'Monthly Coffee Production by Country',
+          },
         },
-      },
+      }
     }
+  },
+  methods: {
+    refresh: function () {
+    },
   },
 }
 </script>

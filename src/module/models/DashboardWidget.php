b8298a2c21781548d71aa6c237f27a9376c69bbc
aneenashaju <aneenashaju@gmail.com>
2019-08-12 12:44:13 +0530

    model

---
+++ src/module/models/DashboardWidget.php
@@ -0,0 +1,52 @@
+<?php
+
+use codexten\yii\db\ActiveRecord;
+use codexten\yii\modules\auth\models\User;
+/**
+ * Class DashboardWidget
+ */
+class DashboardWidget extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%dashboard_widget}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id'], 'required'],
+            [['created_at', 'updated_at', 'status'], 'integer'],
+            [['code', 'data'], 'safe'],
+            [
+                ['user_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => User::class,
+                'targetAttribute' => ['user_id' => 'id'],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('codexten:dashboard', 'ID'),
+            'user_id' => Yii::t('codexten:dashboard', 'User ID'),
+            'code' => Yii::t('codexten:dashboard', 'Code'),
+            'data' => Yii::t('codexten:dashboard', 'Data'),
+            'status' => Yii::t('codexten:dashboard', 'Status'),
+            'created_at' => Yii::t('codexten:dashboard', 'Created At'),
+            'updated_at' => Yii::t('codexten:dashboard', 'Updated At'),
+        ];
+    }
+}

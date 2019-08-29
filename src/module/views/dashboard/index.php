c4d3e0171f451763316d2253ecf9405825734a7b
Jomon Johnson <jomon.entero@gmail.com>
2019-08-29 12:28:07 +0530

    init

--- src/module/views/dashboard/index.php
+++ src/module/views/dashboard/index.php
@@ -4,6 +4,7 @@ use codexten\yii\modules\dashboard\widgets\DashboardPage;
 use yii\web\View;
 
 /* @var $this View */
+/* @var $_params_ array */
 
 $this->title = Yii::t('en:te:company', 'Dashboard');
 $this->params['breadcrumbs'][] = $this->title;
@@ -15,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?php $page->beginContent('content') ?>
 
-<?= $this->render('index/_content') ?>
+<?= $this->render('index/_content', $_params_) ?>
 
 <?php $page->endContent() ?>
 

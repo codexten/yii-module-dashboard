5ac6796965bd557710169ef4be487538b94f09b7
Jomon Johnson <jomon.entero@gmail.com>
2019-09-03 17:33:27 +0530

    init

--- src/asset/.eslintrc.js
+++ src/asset/.eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
+
+    plugins: [
     'html'
   ],
   env: {
